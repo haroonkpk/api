b544c9216e67aa2f57426472622bb40a620c090f
haroonkpk <haroonkhanlala47@gmail.com>
2025-01-04 11:44:06 -0800

    for all

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const {router:userRouter}= require("./user")
 const cors=require("cors")
+const corsConfig = {
+  origin: true,
+  credentials: true, 
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+}
+app.options('', cors(corsConfig))
 const books= require("./book")
 
 const uri =
   "mongodb+srv://haroonkhanlala47:XoHkAxN0MxlGlrwU@store.m82ru.mongodb.net/?retryWrites=true&w=majority&appName=store";
 
-mongoose.connect(uri, {})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+  
+  app.use(cors(corsConfig));
+  app.use(express.json());
+  app.use(express.urlencoded({extended:false}))
+  mongoose.connect(uri, {})
+      .then(() => console.log('MongoDB connected'))
+      .catch(err => console.error('MongoDB connection error:', err));
 
-app.git("/",(req,res)=>{
+
+    app.git("/",(req,res)=>{
     res.send("hello world")
-});
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+  });
 app.use(userRouter);
 app.use(books);
 
